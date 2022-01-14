fc84d24b765fd943e32224987dae432458b53f9a
zulanx <68916048+zulanx@users.noreply.github.com>
2022-01-14 14:18:45 -0600

    Update initFiles.js

--- src/initFiles.js
+++ src/initFiles.js
@@ -1,5 +1,5 @@
 const baseUrl = 'https://raw.githubusercontent.com/zulanx/bitrun/main/src/'
-const filesToDownload = ['1startPurchase.js', 'achieve_edit.js', 'doScan.js', 'easyHack.js', 'findServer.js', 'find_coding_contract.js', 'gainRoot.js', 'getServers.js', 'getServersOrg.js', 'getSF.js', 'host_grow.js', 'host_hack.js', 'host_weak.js', 'killSCRIPT.js', 'mainHack.js', 'populateHome.js', 'populatePserv.js', 'purchaseServer.js', 'readme.txt', 'scanServers.js', 'sendIT.js', 'spreadIT.js', 'start.js', 'stocks.js', 'superSendIT.js', 'superSolver.js', 'watchServer.js', 'writeServerInfo.js']
+const filesToDownload = ['1startPurchase.js', 'achieve_edit.js', 'doScan.js', 'easyHack.js', 'findServer.js', 'find_coding_contract.js', 'gainRoot.js', 'getServers.js', 'getServersOrg.js', 'getSF.js', 'host_grow.js', 'host_hack.js', 'host_weak.js', 'killSCRIPT.js', 'mainHack.js', 'populateHome.js', 'populatePserv.js', 'purchaseServer.js', 'scanServers.js', 'sendIT.js', 'spreadIT.js', 'start.js', 'stocks.js', 'superSendIT.js', 'superSolver.js', 'watchServer.js', 'writeServerInfo.js']
 
 function localeHHMMSS(ms = 0) {
   if (!ms) {
@@ -31,6 +31,6 @@ export async function main(ns) {
 
 
   ns.tprint(`[${localeHHMMSS()}] Spawning spreadIT.js`)
-  ns.spawn('spreadIT.js', 1, 'n00dles', 'easyHack.js')
+  ns.run('spreadIT.js', 1, 'n00dles', 'easyHack.js')
   ns.spawn('1startPurchase.js')
 }
