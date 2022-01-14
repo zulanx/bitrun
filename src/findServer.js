b6f68c41e0c324ab0c15abbde9a44a38dc16a073
zulanx <68916048+zulanx@users.noreply.github.com>
2022-01-14 12:43:32 -0600

    updoots

--- src/findServer.js
+++ src/findServer.js
@@ -1,72 +1,39 @@
 /** @param {import(".").NS} ns **/
 export async function main(ns) {
+	ns.disableLog("ALL");
+	var src = ns.getHostname();
+	var tgt = ns.args[0];
+	var servers = {};
+	var queue = [src];
+	var route = [tgt];
+	var ptr = tgt;
 
-    //Uses a DFS to find the path to the specified server and then prints the path
-    //to Terminal.
-    
-    //The target server's HOSTNAME must be a string passed in as an argument to the script.
-    //It is CASE-SENSITIVE.
-    //If an invalid hostname is passed the script will probably just run forever.
-    
-    if (ns.args.length == 0 ) { ns.tprint(`No arguments supplied, need target`); return 0;}
-    
-    var target = ns.args[0];
-    
-    var visited = [];
-    var stack = [];
-    var parentTracker = [];
-    var origin = ns.getHostname();
-    var node='';
-    var nextNodes =[];
-    var pair=[];
-    var parentTracker=[];
-    var i = '';
-    stack.push(origin);
-    var stack = [];
-    var path=[];
-    
-    while(stack.length > 0) {
-        node = stack.pop();
-        ns.print("DFS processing server " + node);
-        if (visited.includes(node)) {
-            //Do nothing. Essentially a "continue" but that doesn't exist yet
-        } else {
-            if (node == target) {break;}
-            visited.push(node);
-            nextNodes = ns.scan(node);
-            for (i = 0; i < nextNodes.length; ++i) {
-                stack.push(nextNodes[i]);
-    
-                //Keep track of the nodes "parent" so we can re-create the path
-                //Ignore entries that start at the origin
-                if (nextNodes[i] != origin) {
-                    pair = [nextNodes[i], node];
-                    parentTracker.push(pair);
-                }
-            }
-        }
-    }
-    
-    ns.print("Target found. About to re-create path");
-    ns.print("parentTracker size: " + parentTracker.length);
-    path = [];
-    i = target;
-    while (i != ns.getHostname()) {
-        path.push(i);
-        ns.print("Re-creating path at " + i);
-    
-        //Search through the parentTracker array to find this nodes parent
-        for (let j = 0; j < parentTracker.length; ++j) {
-            pair = parentTracker[j];
-            if (pair[0] == i) {
-                i = pair[1];
-                break;
-            }
-        }
-    }
-    
-    path.reverse();
-    ns.tprint(path);
-    
-    
-    }
+	servers[src] = "START"
+
+	while (queue.length) {
+
+
+
+		var s = queue.pop();
+
+
+		var links = ns.scan(s);
+		for (var i = 0; i < links.length; i++) {
+			if (!servers[links[i]]) {
+				queue.push(links[i]);
+				servers[links[i]] = s;
+			}
+		};
+		await ns.sleep(10);
+	}
+
+	while (ptr != src) {
+		ptr = servers[ptr];
+		route.push(ptr);
+		await ns.sleep(10);
+	}
+	route.pop();
+
+	ns.print(route.reverse());
+	ns.tprint(route.reverse());
+}
--- src/initFilesTest.js
+++ src/initFilesTest.js
@@ -1,6 +1,6 @@
 const baseUrl = 'https://raw.githubusercontent.com/zulanx/bitrun/main/src/'
 const filesToDownload = 
-['1startPurchase.js', 'achieve_edit.js', 'bitrun-sublime.sublime-workspace', 'doScan.js', 'easyHack.js', 'findServer.js', 'find_coding_contract.js', 'gainRoot.js', 'getServers.js', 'getServersOrg.js', 'getSF.js', 'host_grow.js', 'host_hack.js', 'host_weak.js', 'index.d.ts', 'initFiles.ns', 'initFilesTest.ns', 'killSCRIPT.js', 'mainHack.js', 'populateHome.js', 'populatePserv.js', 'purchaseServer.js', 'readme.txt', 'rollup.py', 'scanServers.js', 'sendIT.js', 'spreadIT.js', 'start.ns', 'stocks.js', 'superSendIT.js', 'superSolver.js', 'watchServer.js', 'writeServerInfo.js']
+['1startPurchase.js', 'achieve_edit.js', 'doScan.js', 'easyHack.js', 'findServer.js', 'find_coding_contract.js', 'gainRoot.js', 'getServers.js', 'getServersOrg.js', 'getSF.js', 'host_grow.js', 'host_hack.js', 'host_weak.js', 'initFiles.js', 'initFilesTest.js', 'killSCRIPT.js', 'mainHack.js', 'populateHome.js', 'populatePserv.js', 'purchaseServer.js', 'readme.txt', 'scanServers.js', 'sendIT.js', 'spreadIT.js', 'start.js', 'stocks.js', 'superSendIT.js', 'superSolver.js', 'watchServer.js', 'writeServerInfo.js']
 
 function localeHHMMSS(ms = 0) {
   if (!ms) {
